55f4b28d11c7bae4c74e2a14543c9481184d7713
Kevin Borduas <kevin.borduas@gsoft-group.com>
2019-12-20 10:52:02 -0500

    next and nexter tiles

--- bomberjam-starters-master/csharp/RawSmartBot.cs
+++ bomberjam-starters-master/csharp/RawSmartBot.cs
@@ -34,22 +34,41 @@ namespace Bomberjam.Bot
             var topTile = (uint) GameStateUtils.GetBoardTile(state, x, y - 1, myPlayerId);
             var leftTile = (uint) GameStateUtils.GetBoardTile(state, x - 1, y, myPlayerId);
             var rightTile = (uint) GameStateUtils.GetBoardTile(state, x + 1, y, myPlayerId);
-            var bottomTile = (uint) GameStateUtils.GetBoardTile(state, x - 1, y + 1, myPlayerId);
+            var bottomTile = (uint) GameStateUtils.GetBoardTile(state, x, y + 1, myPlayerId);
+
+            var nextTopTile = (uint) GameStateUtils.GetBoardTile(state, x, y - 2, myPlayerId);
+            var nextLeftTile = (uint) GameStateUtils.GetBoardTile(state, x - 2, y, myPlayerId);
+            var nextRightTile = (uint) GameStateUtils.GetBoardTile(state, x + 2, y, myPlayerId);
+            var nextBottomTile = (uint) GameStateUtils.GetBoardTile(state, x, y + 2, myPlayerId);
+
+            var nexterTopTile = (uint) GameStateUtils.GetBoardTile(state, x, y - 3, myPlayerId);
+            var nexterLeftTile = (uint) GameStateUtils.GetBoardTile(state, x - 3, y, myPlayerId);
+            var nexterRightTile = (uint) GameStateUtils.GetBoardTile(state, x + 3, y, myPlayerId);
+            var nexterBottomTile = (uint) GameStateUtils.GetBoardTile(state, x, y + 3, myPlayerId);
 
             var amIOnABomb = GameStateUtils.GetBoardTile(state, x, y, myPlayerId) == GameStateUtils.Tile.Bomb;
 
             var otherPlayerInRange = state.Players.Any(z => (Math.Abs(z.Value.X) - Math.Abs(player.X)) < player.BombRange || (Math.Abs(z.Value.Y) - Math.Abs(player.Y)) < player.BombRange);
             var playerInBombRange = state.Bombs.Any(z => (player.X - z.Value.X) <= z.Value.Range || (player.Y - z.Value.Y) <= z.Value.Range);
-
+            
             var features = new List<float>
             {
                 topTile,
                 leftTile,
                 rightTile,
                 bottomTile,
+
+                nextTopTile,
+                nextLeftTile,
+                nextRightTile,
+                nextBottomTile,
+
+                nexterTopTile,
+                nexterLeftTile,
+                nexterRightTile,
+                nexterBottomTile,
+
                 amIOnABomb ? 1 : 0,
-
-
 
                 otherPlayerInRange ? 1 : 0,
                 playerInBombRange ? 1 : 0,
